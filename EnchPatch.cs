c1912c29a5f70b946147cef87a397206d152567b
jason0320 <jasonnosaj0320@gmail.com>
2024-12-14 15:24:10 +0800

    加入專案檔案。

---
+++ EnchPatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_PandaUncapEnchantMod
+{
+    internal class EnchPatch
+    {
+        public static void OnStartCore()
+        {
+            SourceManager sources = Core.Instance.sources;
+            foreach (SourceElement.Row row in sources.elements.rows)
+            {
+                EnchPatch.FeatRewrite(row);
+            }
+        }
+        public static void FeatRewrite(SourceElement.Row ele)
+        {
+            if (ele.group == "ENC" && ele.alias == "encSpell")
+            {
+                ele.SetField("encFactor", 100);
+            }
+            if (ele.group == "ENC" && ele.alias == "travelSpeed")
+            {
+                ele.SetField("encFactor", 100);
+            }
+        }
+    }
+}
---
+++ Mod_PandaUncapEnchantMod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+
+namespace Mod_PandaUncapEnchantMod
+{
+    [BepInPlugin("panda.uncapenchant.mod", "Panda's Uncap Enchant Mod", "1.0.0.0")]
+    internal class Mod_PandaUncapEnchantMod : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            var harmony = new Harmony("Panda's Uncap Enchant Mod");
+            harmony.PatchAll();
+        }
+        public void OnStartCore()
+        {
+            EnchPatch.OnStartCore();
+        }
+    }
+}
